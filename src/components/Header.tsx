290435b19f683458fb66b8f925806428aa38be32
GuuhaN <nigelcheung97@hotmail.com>
2025-01-07 01:30:48 +0100

    first setup portfolio

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="flex flex-row justify-between py-4">
+      <Link href="/" className="text-xl font-bold tracking-wider">
+        NC.
+      </Link>
+      <ul className="flex flex-row gap-12">
+        <li>
+          <Link href="/work">Work</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>-</li>
+      </ul>
+    </nav>
+  );
+}
