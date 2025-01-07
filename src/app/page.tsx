67af3f5876f5ae27ad2c8b18f2a1d3d98ffba2bc
GuuhaN <nigelcheung97@hotmail.com>
2025-01-07 01:33:16 +0100

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="flex flex-col gap-4">
       <div className="flex flex-row justify-between">
         <div className="bg-white h-20 w-10"></div>
-        <div className="bg-slate-100 h-40 w-20"></div>
-        <div className="bg-slate-200 h-60 w-28"></div>
+        <div className="bg-slate-50 h-40 w-20"></div>
+        <div className="bg-slate-100 h-60 w-28"></div>
       </div>
       <div className="flex flex-row gap-8 basis-1">
         <div>
-          <p>(Hello Nigel)</p>
+          <p>(Hello NC.)</p>
           <h1 className="md:text-8xl lg:text-9xl font-semibold tracking-wide">
             Full <span className="font-normal italic">stack</span> Developer
           </h1>
@@ -21,9 +21,7 @@ export default function Home() {
             <span className="font-semibold"> work</span>.
           </p>
           <Link href="mailto:nigelcheung97@hotmail.com" className="flex ">
-            <div className="border rounded-full py-2 px-4">
-              Let's discuss <span></span>
-            </div>
+            <div className="border rounded-full py-2 px-4">Lets discuss</div>
           </Link>
         </div>
       </div>
