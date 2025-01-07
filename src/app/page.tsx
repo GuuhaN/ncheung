7663b115849fc6f18e37dd1f08f8c1165592dcba
GuuhaN <nigelcheung97@hotmail.com>
2025-01-07 01:40:59 +0100

    small improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
         <div className="bg-slate-50 h-40 w-20"></div>
         <div className="bg-slate-100 h-60 w-28"></div>
       </div>
-      <div className="flex flex-row gap-8 basis-1">
-        <div>
+      <div className="flex flex-col gap-20 md:flex-row">
+        <div className="basis-2/3 w-full">
           <p>(Hello NC.)</p>
-          <h1 className="md:text-8xl lg:text-9xl font-semibold tracking-wide">
+          <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-semibold tracking-wider w-full text-justify">
             Full <span className="font-normal italic">stack</span> Developer
           </h1>
         </div>
-        <div className="flex flex-col w-full gap-2 place-content-end">
+        <div className="flex flex-col w-full gap-2 place-content-end basis-1/3 text-2xl tracking-wide">
           <p className="text-justify">
             As a fullstack developer, we create applications that
             <span className="font-semibold"> work</span>.
